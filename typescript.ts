d8cdce65435f0d7c869f5454509313635f3b7867
Max Gagner <m.gagner@hotmail.se>
2024-03-28 17:04:17 +0100

    Initiall html and css done, code step started

---
+++ typescript.ts
@@ -0,0 +1,21 @@
+interface course {
+    code: string,
+    name: string,
+    progression: string,
+    syllabus: string
+}
+
+function load() : void {
+    addEventListener
+}
+
+function add() : void {
+
+}
+
+function update() : void {
+
+}
+
+
+window.onload = load;
